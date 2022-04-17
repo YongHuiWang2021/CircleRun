be7f15286fde1c4704f8f1e3240b3cdf76bb169f
YHWang2021 <yonghuiw@crazylabs.com>
2022-04-17 11:50:38 +0800

    mergy puzzle

--- Assets/XD/Scripts/CameraController.cs
+++ Assets/XD/Scripts/CameraController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-    public Transform target;
-    public float lerp = 5.0f;
+namespace XD.Game
+{
+    public class CameraController : MonoBehaviour {
+        public Transform target;
+        public float lerp = 5.0f;
 
-	void LateUpdate () {
-        //Lerp Towards the target
-        if (target)
-            transform.position = Vector3.Lerp(transform.position, new Vector3 (target.position.x, target.position.y,transform.position.z), Time.deltaTime * lerp);
-        else
-            Debug.LogError("Please Assign all the variables");
+        void LateUpdate () {
+            //Lerp Towards the target
+            if (target)
+                transform.position = Vector3.Lerp(transform.position, new Vector3 (target.position.x, target.position.y,transform.position.z), Time.deltaTime * lerp);
+            else
+                Debug.LogError("Please Assign all the variables");
 
-	}
+        }
+    }
+
 }
